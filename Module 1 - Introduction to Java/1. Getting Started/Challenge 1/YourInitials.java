ea59241b2b9324aa445bbf4551891442fb8728b7
maclamour <maclamour@gmail.com>
2023-01-15 09:26:37 -0500

    printed initials using system.out.println in Java

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,12 +1,12 @@
 public class YourInitials {
     public static void main(String[] args) {
          // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
-         System.out.println("MMMMM  LLLLL");
-         System.out.println("MMMMM  LLLLL");
-         System.out.println("MMMMM  LLLLL");
-         System.out.println("MMMMM  LLLLL");
-         System.out.println("MMMMM  LLLLL");
-         System.out.println("MMMMM  LLLLL");
-         System.out.println("MMMMM  LLLLL");
+         System.out.println("M   M  L    ");
+         System.out.println("MM MM  L    ");
+         System.out.println("M M M  L    ");
+         System.out.println("M   M  L    ");
+         System.out.println("M   M  L    ");
+         System.out.println("M   M  L    ");
+         System.out.println("M   M  LLLLL");
     }
 }
