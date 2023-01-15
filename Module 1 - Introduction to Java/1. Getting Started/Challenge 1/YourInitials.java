5bb3f21d210c2cb4669499a15c30ac05a2b39543
maclamour <maclamour@gmail.com>
2023-01-15 09:19:50 -0500

    printing intials

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,6 +1,12 @@
 public class YourInitials {
     public static void main(String[] args) {
          // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
-         System.out.println("Welcome to your initials ML");
+         System.out.println("MMMMM  LLLLL");
+         System.out.println("MMMMM  LLLLL");
+         System.out.println("MMMMM  LLLLL");
+         System.out.println("MMMMM  LLLLL");
+         System.out.println("MMMMM  LLLLL");
+         System.out.println("MMMMM  LLLLL");
+         System.out.println("MMMMM  LLLLL");
     }
 }
