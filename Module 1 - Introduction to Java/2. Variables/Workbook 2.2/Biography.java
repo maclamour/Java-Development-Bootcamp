9addc176967bf93497e9cb349e1d3de4c840c998
maclamour <maclamour@gmail.com>
2023-01-17 09:34:06 -0500

    Biography.java

--- Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
@@ -21,10 +21,10 @@ public class Biography {
         // grade (character)
         char grade = 'A';
      
-        System.out.println("My name is " + name + "." + "I'm" + age +  " years old, and I'm from" + country);
-        System.out.println("My favourite sport is" + sport + "." + "I play for" + hours + "hours a day");
-        System.out.println("When I'm tired, I like to play" + game + ".");
-        System.out.println("In school, my favourite subject was" + subject + "," + "I scored a" + grade +".");
+        System.out.println("My name is " + name + "." + " I'm " + age +  " years old, and I'm from " + country);
+        System.out.println("My favorite sport is " + sport + "." + " I play for " + hours + " hours a day");
+        System.out.println("When I'm tired, I like to play " + game + ".");
+        System.out.println("In school, my favourite subject was " + subject + "," + "I scored an " + grade +".");
 
         // Compare your result to what's on Learn the Part.
     }
