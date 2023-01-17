0ede06f9523468e22fb8cf96af82643a06f1e56d
maclamour <maclamour@gmail.com>
2023-01-17 08:23:34 -0500

    points.java practice

--- Module 1 - Introduction to Java/2. Variables/Workbook 2.1/Points.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.1/Points.java
@@ -5,16 +5,20 @@ public class Points {
 
         int points = 0;
 
+
+
         System.out.println("Harry was caught wandering the halls. -50 points for Gryffindor");
-        //update points here
+        points -= 50;
         System.out.println("Harry was being cheeky in class. -3 points for Gryffindor");
-        //update points here
+        points -= 3;
         System.out.println("Hermione got full marks on Lockhart's quiz. 30 points for Gryffindor");
-        //update points here
+        points += 30;
         System.out.println("Ron won the underground chess game. 100 points for Gryffindor");
-        //update points here
+        points += 100;
         System.out.println("Harry defeated Quirrell. 60 points for Gryffindor");
-        //update points here        
+        points += 60;   
+
+        System.out.println(points);
         
         // Compare your result to what's on Learn the Part.
         
