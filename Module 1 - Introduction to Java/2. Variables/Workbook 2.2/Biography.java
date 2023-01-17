adba42f6998d002d97e35d88536300444765e6d0
maclamour <maclamour@gmail.com>
2023-01-17 09:31:23 -0500

    Biography.java

--- Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
@@ -5,18 +5,26 @@ public class Biography {
 
 
         // name (text)
+        String name = "Mackenzie";
         // age (whole number)
+        int age = 31;
         // country (text)
+        String country = "Haiti";
         // sport (text)
+        String sport = "Football";
         // hours (whole number)
+        int hours = 3;
         // game (text)
+        String game = "Warzone";
         // subject (text)
+        String subject = "Math";
         // grade (character)
+        char grade = 'A';
      
-        System.out.println("My name is <name>. I'm <age> years old, and I'm from <country> ");
-        System.out.println("My favourite sport is <sport>. I play for <hours> hours a day");
-        System.out.println("When I'm tired, I like to play <game>.");
-        System.out.println("In school, my favourite subject was <subject>, I scored a <grade>.");
+        System.out.println("My name is " + name + "." + "I'm" + age +  " years old, and I'm from" + country);
+        System.out.println("My favourite sport is" + sport + "." + "I play for" + hours + "hours a day");
+        System.out.println("When I'm tired, I like to play" + game + ".");
+        System.out.println("In school, my favourite subject was" + subject + "," + "I scored a" + grade +".");
 
         // Compare your result to what's on Learn the Part.
     }
---
+++ Section-2/Grades.java
@@ -0,0 +1,15 @@
+public class Grades {
+    public static void main (String[] args){
+
+        char potions = 'A';
+        char artithmancy = 'B';
+        char charms = 'C';
+
+        System.out.println("Potions " + potions);
+        System.out.println("Arithmancy " + artithmancy);
+        System.out.println("Charms " + charms);
+
+
+    }
+    
+}
