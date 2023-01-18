1ac2a671e22b33fb0232d589c0184b1e6a586f8f
maclamour <maclamour@gmail.com>
2023-01-18 09:59:18 -0500

    WholeNumbers.java

--- Section-2/WholeNumbers.java
+++ Section-2/WholeNumbers.java
@@ -1,8 +1,10 @@
 public class WholeNumbers {
     public static void main(String[] args){
         long globalPopulation = 8000000000L;
+        System.out.println("The global population is " + globalPopulation);
+        
         long dailyGoogleSearches = 8500000000L;
-        System.out.println(globalPopulation);
-        System.out.println(dailyGoogleSearches);
+        
+        System.out.println("The daily number of google searches is " + dailyGoogleSearches);
     }
 }
