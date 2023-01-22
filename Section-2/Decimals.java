5631984da528a1501d9412032c2450b280547bdc
maclamour <maclamour@gmail.com>
2023-01-22 09:58:21 -0500

    Decinmals.java

---
+++ Section-2/Decimals.java
@@ -0,0 +1,11 @@
+public class Decimals {
+    public static void main(String[] args){
+        double percentage = 73.5;
+        System.out.println(percentage + " of percentages are made up.");
+
+        double wallet = 20; // always use doubles for math calculations. This will store as a double: 20.0000
+        int people = 3;
+        System.out.println(wallet / people);
+    }
+    
+}
