4a7249d5810f4d4f3212b61dff19e46e40e077ba
maclamour <maclamour@gmail.com>
2023-01-31 14:15:25 -0500

    Chatbot.java

--- Module 1 - Introduction to Java/2. Variables/Challenge 2/Chatbot.java
+++ Module 1 - Introduction to Java/2. Variables/Challenge 2/Chatbot.java
@@ -8,26 +8,26 @@ public class Chatbot {
         //See Learn the Part for the complete instructions. 
 
         
-       Scanner scanner = new Scanner(System.in);
+       Scanner scan = new Scanner(System.in);
 
         System.out.println("Hello. What is your name?");
         //Pick up user's name and store it. 
-        String name = scanner.nextLine();
+        String name = scan.nextLine();
 
                            
         //add new a line before asking next question. 
         System.out.println( "\nHi " + name + "!" + " I'm Javabot. Where are you from?");
         //Pick up user's home and store it. 
-        String home = scanner.nextLine();
+        String home = scan.nextLine();
                         
 
         //add new a line before asking next question.
         System.out.println("\nI hear it's beautiful at " + home + "!" +" I'm from a place called Oracle");
         System.out.println("\nHow old are you?");
         //Pick up age and store it.
-        int age = scanner.nextInt();
+        int age = scan.nextInt();
 
-        scanner.nextLine();
+        
 
 
         //add new a line before asking next question.
@@ -35,13 +35,14 @@ public class Chatbot {
         System.out.println("\nThis means I'm " + (400 / age) +  " times older than you.");
         System.out.println("\nEnough about me. What's your favourite language? (just don't say Python)");
         //Pick up language and store it. 
-        String language = scanner.nextLine();
+        scan.nextLine();
+        String language = scan.nextLine();
 
 
         //add new a line here.
         System.out.println("\n" +language +  "that's great! Nice chatting with you" +  name + "." + "I have to log off now. See ya!");
         
-        scanner.close();
+        scan.close();
 
         
     }
