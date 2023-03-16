a7f0e39d966b3d59c2cc65575acb02de316d1cc5
maclamour <maclamour@gmail.com>
2023-03-15 22:00:28 -0400

    fizz.java

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.7 Solution/FizzBuzz.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.7 Solution/FizzBuzz.java
@@ -1,7 +1,7 @@
 public class FizzBuzz {
     public static void main(String[] args) {
 
-        for (int i = 0; i < 19; i++) {
+        for (int i = 0; i < 25; i++) {
             
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println(i + " FizzBuzz");
